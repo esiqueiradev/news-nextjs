622316e469b6f5a3eadae026ab2172271a464a00
Eduardo Siqueira <esiqueira.dev@gmail.com>
2021-11-18 18:07:09 -0300

    Finish project structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
-import styles from '../styles/home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Home page</h1>
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Home page</h1>
+    </>
   )
 }
