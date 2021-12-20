669fabc1fac8a90d1f8aa26e937164b5ae23717b
Eduardo Siqueira <esiqueira.dev@gmail.com>
2021-12-20 17:18:32 -0300

    Add auth with faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query } from "faunadb"
+
+import { fauna } from "../../../services/fauna"
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,4 +17,40 @@ export default NextAuth({
       }
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index("user_by_email"),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection("users"),
+              {
+                data: {
+                  email: user.email,
+                }
+              }
+            ),
+            query.Get(
+              query.Match(
+                query.Index("user_by_email"),
+                query.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false;
+      }
+    },
+  }
 })
